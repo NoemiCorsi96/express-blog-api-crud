1da9c3baa8fa37ae9c1dd4d5af2c637c2757b74d
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-09-12 15:41:49 +0200

    do Milestone 2 + postman

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,47 +1,11 @@
 //Milestone 1
-// creo l'array con id
-const posts = [
-  {
-    id: 1,
-    titolo: "Ciambellone",
-    contenuto: "Un classico dolce soffice da colazione, semplice e genuino.",
-    immagine: "/img/ciambellone.jpeg",
-    tags: ["dolci", "colazione", "cucina"]
-  },
-  {
-    id: 2,
-    titolo: "Cracker alla Barbabietola",
-    contenuto: "Snack croccanti e colorati, perfetti come aperitivo o spezza-fame.",
-    immagine: "/img/cracker_barbabietola.jpeg",
-    tags: ["snack", "salati", "barbabietola"]
-  },
-  {
-    id: 3,
-    titolo: "Pane Fritto Dolce",
-    contenuto: "Ricetta tradizionale: pane fritto arricchito con zucchero, semplice ma irresistibile.",
-    immagine: "/img/pane_fritto_dolce.jpeg",
-    tags: ["tradizione", "dolci", "frittura"]
-  },
-  {
-    id: 4,
-    titolo: "Pasta alla Barbabietola",
-    contenuto: "Un primo piatto colorato e ricco di gusto, con tutta la dolcezza della barbabietola.",
-    immagine: "/img/pasta_barbabietola.jpeg",
-    tags: ["primi", "barbabietola", "cucina"]
-  },
-  {
-    id: 5,
-    titolo: "Torta Paesana",
-    contenuto: "Dolce rustico tipico lombardo a base di pane raffermo, latte e cacao.",
-    immagine: "/img/torta_paesana.jpeg",
-    tags: ["tradizione", "dolci", "lombardia"]
-  }
-];
+
+const posts= require('../data/post')
 
 //index 
 const index= (req, res) => {
 
-    res.send("Lista di posts")
+    res.json(posts)
 }
 
 //show
@@ -85,7 +49,25 @@ const modify= (req, res) => {
 //destroy
 
 const destroy= (req, res) => {
-  res.send('Delete the single post with ID:' + req.params.id)
+  const { id } = req.params
+  console.log(id);
+
+
+  const post = posts.find(item => item.id === parseInt(id))
+  console.log(post);
+
+  if (!post) {
+
+    return res.status(404).json({
+      error: true,
+      message: 'Resource not found'
+    })
+  }
+
+  posts.splice(posts.indexOf(post), 1);
+  console.log(posts);
+
+  res.sendStatus(204)
 }
 
 
