3e41afcd4825a0108abbe10f51849f3269d88a69
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-09-12 15:26:11 +0200

    create Controller

---
+++ controllers/postController.js
@@ -0,0 +1,100 @@
+//Milestone 1
+// creo l'array con id
+const posts = [
+  {
+    id: 1,
+    titolo: "Ciambellone",
+    contenuto: "Un classico dolce soffice da colazione, semplice e genuino.",
+    immagine: "/img/ciambellone.jpeg",
+    tags: ["dolci", "colazione", "cucina"]
+  },
+  {
+    id: 2,
+    titolo: "Cracker alla Barbabietola",
+    contenuto: "Snack croccanti e colorati, perfetti come aperitivo o spezza-fame.",
+    immagine: "/img/cracker_barbabietola.jpeg",
+    tags: ["snack", "salati", "barbabietola"]
+  },
+  {
+    id: 3,
+    titolo: "Pane Fritto Dolce",
+    contenuto: "Ricetta tradizionale: pane fritto arricchito con zucchero, semplice ma irresistibile.",
+    immagine: "/img/pane_fritto_dolce.jpeg",
+    tags: ["tradizione", "dolci", "frittura"]
+  },
+  {
+    id: 4,
+    titolo: "Pasta alla Barbabietola",
+    contenuto: "Un primo piatto colorato e ricco di gusto, con tutta la dolcezza della barbabietola.",
+    immagine: "/img/pasta_barbabietola.jpeg",
+    tags: ["primi", "barbabietola", "cucina"]
+  },
+  {
+    id: 5,
+    titolo: "Torta Paesana",
+    contenuto: "Dolce rustico tipico lombardo a base di pane raffermo, latte e cacao.",
+    immagine: "/img/torta_paesana.jpeg",
+    tags: ["tradizione", "dolci", "lombardia"]
+  }
+];
+
+//index 
+const index= (req, res) => {
+
+    res.send("Lista di posts")
+}
+
+//show
+const show= (req, res) => {
+
+  const { id } = req.params
+  console.log(id);
+
+
+  const post = posts.find(item => item.id === parseInt(id))
+  console.log(post);
+  
+  if(!post) {
+
+    res.status(404).json({
+      error: true,
+      message: 'Resource not found'
+    })
+  }
+
+  //res.send('Show the single pizza with ID:' + req.params.id)
+  res.json(post)
+
+}
+
+//store 
+const store= (req, res) => {
+    res.send('Create a new post')
+}
+
+//update 
+const update= (req, res) => {
+  res.send('Update the entire single post with ID:' + req.params.id)
+}
+
+//modify 
+const modify= (req, res) => {
+  res.send('Partial update for the single post with ID:' + req.params.id)
+}
+
+//destroy
+
+const destroy= (req, res) => {
+  res.send('Delete the single post with ID:' + req.params.id)
+}
+
+
+//esporto le azioni 
+module.exports = {
+    index,
+    show,
+    store,
+    update,
+    modify,
+    destroy
+}
--- routers/posts.js
+++ routers/posts.js
@@ -3,98 +3,30 @@ const express = require('express');
 //creo l'istanza principale
 const router = express.Router()
 
-//vado a copiare tutte le rotte farre in server.js, cambio app=>router
-// creo l'array con id
-const posts = [
-  {
-    id: 1,
-    titolo: "Ciambellone",
-    contenuto: "Un classico dolce soffice da colazione, semplice e genuino.",
-    immagine: "/img/ciambellone.jpeg",
-    tags: ["dolci", "colazione", "cucina"]
-  },
-  {
-    id: 2,
-    titolo: "Cracker alla Barbabietola",
-    contenuto: "Snack croccanti e colorati, perfetti come aperitivo o spezza-fame.",
-    immagine: "/img/cracker_barbabietola.jpeg",
-    tags: ["snack", "salati", "barbabietola"]
-  },
-  {
-    id: 3,
-    titolo: "Pane Fritto Dolce",
-    contenuto: "Ricetta tradizionale: pane fritto arricchito con zucchero, semplice ma irresistibile.",
-    immagine: "/img/pane_fritto_dolce.jpeg",
-    tags: ["tradizione", "dolci", "frittura"]
-  },
-  {
-    id: 4,
-    titolo: "Pasta alla Barbabietola",
-    contenuto: "Un primo piatto colorato e ricco di gusto, con tutta la dolcezza della barbabietola.",
-    immagine: "/img/pasta_barbabietola.jpeg",
-    tags: ["primi", "barbabietola", "cucina"]
-  },
-  {
-    id: 5,
-    titolo: "Torta Paesana",
-    contenuto: "Dolce rustico tipico lombardo a base di pane raffermo, latte e cacao.",
-    immagine: "/img/torta_paesana.jpeg",
-    tags: ["tradizione", "dolci", "lombardia"]
-  }
-];
+//importo il Controller
+const postController= require('../controllers/postController')
 
-//lista post 
-router.get('', (req, res) => {
-    //cosi restituisco la frase 'lista dei post'
-    //res.send('Lista dei post')
-    //con il comando seguente lo visualizzo in formato json
-    res.json(posts)
-})
+
+
+//lista post index
+router.get('', postController.index)
 
 //show 
-router.get('/:id', (req, res) => {
-
-  const { id } = req.params
-  console.log(id);
-
-
-  const post = posts.find(item => item.id === parseInt(id))
-  console.log(post);
-  
-  if(!post) {
-
-    res.status(404).json({
-      error: true,
-      message: 'Resource not found'
-    })
-  }
-
-  //res.send('Show the single pizza with ID:' + req.params.id)
-  res.json(post)
-
-})
+router.get('/:id', postController.show)
 
 
 //store
 
-router.post('', (req, res) => {
-    res.send('Create a new post')
-})
+router.post('', postController.store)
 
 // update
-router.put('/:id', (req, res) => {
-  res.send('Update the entire single post with ID:' + req.params.id)
-})
+router.put('/:id', postController.update)
 
 // modify (U)
-router.patch('/:id', (req, res) => {
-  res.send('Partial update for the single post with ID:' + req.params.id)
-})
+router.patch('/:id',postController.modify )
 
 // destroy (D)
-router.delete('/:id', (req, res) => {
-  res.send('Delete the single post with ID:' + req.params.id)
-})
+router.delete('/:id', postController.destroy)
 
 
 
